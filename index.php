699606d9e381e0d2d1b6136caa4d7d6446c0f8ae
a <a>
2016-02-24 15:00:56 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1513911
+ * Date: 24/02/2016
+ * Time: 14:43
+ */
+?>
+<!DOCTYPE html>
+    <html>
+    <head>
+    </head>
+        <body>
+             <p>
+                <?php
+                 echo "Hello World";
+                 ?>
+             </p>
+         </body>
+
+    </html>
+
